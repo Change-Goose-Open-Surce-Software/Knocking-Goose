676026d8d38bcdf408789153d0914f43f1d54c04
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2025-12-20 18:25:49 +0100

    Initial commit

---
+++ knock.py
@@ -0,0 +1,82 @@
+# knock.py
+import json
+import os
+import sys
+import argparse
+import threading
+import time
+from playsound import playsound
+import pyudev
+
+# Konfiguration laden oder erstellen
+def load_config():
+    config_file = 'config.json'
+    default_config = {
+        'general_sound_connect': None,
+        'general_sound_disconnect': None,
+        'device_specific_sounds': {}
+    }
+    if os.path.exists(config_file):
+        with open(config_file, 'r') as f:
+            return json.load(f)
+    else:
+        with open(config_file, 'w') as f:
+            json.dump(default_config, f)
+        return default_config
+
+# Sound abspielen
+def play_sound(sound_file):
+    if sound_file and os.path.exists(sound_file):
+        playsound(sound_file)
+
+# USB-Geräte überwachen (Beispiel für Linux mit pyudev)
+def monitor_usb():
+    context = pyudev.Context()
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by('usb')
+    config = load_config()
+
+    def handle_device_event(action, device):
+        device_id = device.get('ID_SERIAL', 'default')
+        if action == 'add':
+            sound_file = config['device_specific_sounds'].get(device_id, {}).get('connect', config['general_sound_connect'])
+        else:
+            sound_file = config['device_specific_sounds'].get(device_id, {}).get('disconnect', config['general_sound_disconnect'])
+        play_sound(sound_file)
+
+    for device in iter(monitor.poll, None):
+        if device.action == 'add':
+            handle_device_event('add', device)
+        elif device.action == 'remove':
+            handle_device_event('remove', device)
+
+# Terminal-Oberfläche
+def main():
+    parser = argparse.ArgumentParser(description='Knock - USB Device Sound Notifier')
+    parser.add_argument('--help', action='store_true', help='Show this help message')
+    parser.add_argument('--man', action='store_true', help='Show manual')
+    args = parser.parse_args()
+
+    if args.help:
+        print("Usage: knock [OPTIONS]")
+        print("Options:")
+        print("  --help    Show this help message")
+        print("  --man     Show manual")
+    elif args.man:
+        print("Manual for Knock:")
+        print("  knock --help: Show help message")
+        print("  knock --man: Show this manual")
+    else:
+        # Starte den Hintergrundprozess zur Überwachung von USB-Geräten
+        monitor_thread = threading.Thread(target=monitor_usb)
+        monitor_thread.daemon = True
+        monitor_thread.start()
+        print("Knock is running in the background. Press Ctrl+C to stop.")
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("Stopping Knock...")
+
+if __name__ == '__main__':
+    main()
