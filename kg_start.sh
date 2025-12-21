85efc2f50877212d2b28801dbcaf1e79c1fb8608
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2025-12-21 16:28:13 +0100

    Create kg_start.sh to install Knock application
    This script downloads and installs the Knock application.

---
+++ kg_start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo wget https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Knock/main/install-knocking-goose-linux.sh -O /usr/bin/kg_install-knocking-goose-linux.sh
+
+sudo chmod +x /usr/bin/kg_install-knocking-goose-linux.sh
+
+sudo /usr/bin/kg_install-knocking-goose-linux.sh
+
+kg
