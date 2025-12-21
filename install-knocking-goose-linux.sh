42de627bbff4f41fb0425607926d8623fa22bd58
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2025-12-21 10:27:00 +0100

    Install all dependencies using apt

--- install-knocking-goose-linux.sh
+++ install-knocking-goose-linux.sh
@@ -13,20 +13,14 @@ wget https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Knock/ma
 wget https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Knock/main/knocking-goose.desktop -O knocking-goose.desktop
 wget https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Knock/main/knocking-goose-icon.png -O knocking-goose-icon.png
 
-# Install dependencies using apt only
+# Install dependencies
 echo "Installing dependencies..."
 sudo apt-get update
-sudo apt-get install -y \
-    python3 \
-    python3-tk \
-    python3-gi \
-    python3-pyudev \
-    gir1.2-gstreamer-1.0 \
-    gstreamer1.0-plugins-base \
-    gstreamer1.0-plugins-good \
-    gstreamer1.0-plugins-bad \
-    gstreamer1.0-plugins-ugly \
-    gstreamer1.0-tools
+sudo apt-get install -y python3 python3-tk python3-gi gir1.2-gstreamer-1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly python3-dev python3-setuptools python3-wheel python3-pip libudev-dev
+
+# Install pyudev using pip
+echo "Installing pyudev using pip..."
+pip3 install pyudev
 
 # Copy files to the appropriate locations
 echo "Copying files..."
@@ -43,4 +37,3 @@ mkdir -p ~/.config/autostart
 cp knocking-goose.desktop ~/.config/autostart/
 
 echo "Installation completed successfully."
-echo "You can now run 'kg' from the terminal or start 'Knocking Goose' from your application menu."
